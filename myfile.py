a0f584b8d176c8a524fa7a215e597dfc5f54d7a0
Prismly <40281748+Prismly@users.noreply.github.com>
2025-01-20 19:05:55 +0000

    Reintroduced failing test, Code comments
    - Added code comments to 'tests.py' and 'myfile.py'
    - Reintroduced failing test in 'tests.py' now that
    pytest has been troubleshooted

--- myfile.py
+++ myfile.py
@@ -1,10 +1,13 @@
+# Adds the two given numbers together, then returns the result.
 def add_numbers(a, b):
-    return a + b  
+    return a + b
+
 x, y = 10, 20
+
 if __name__ == "__main__":
     result = add_numbers(x, y)
-    print("The sum of x and y is:", result)
+    print("The sum of x and y is: ", result)
 
-    # Intentional error: calling a function that doesn't exist
+    # Intentional error: calling a function that doesn't exist.
     difference = subtract_numbers(x, y)
-    print("The difference between x and y is:", difference)
+    print("The difference between x and y is: ", difference)
--- tests.py
+++ tests.py
@@ -1,11 +1,10 @@
 from myfile import add_numbers
 
+# Adds two numbers together and expects the correct result.
 def test_add_numbers_pass():
-    # This test will pass
     assert add_numbers(10, 20) == 30
 
+# Adds two numbers together and expects an INCORRECT result.
+# This test intentionally fails, to test automated testing.
 def test_add_numbers_fail():
-    # This test will fail intentionally
-    # Temporarily correcting this test to verify if autotest badge changes to "Success"
-    assert add_numbers(10, 20) == 30
-    # assert add_numbers(10, 20) == 25  # Incorrect expected result
+    assert add_numbers(10, 20) == 25  # Incorrect expected result -- should be 30.
